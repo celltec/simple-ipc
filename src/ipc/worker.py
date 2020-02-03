67252fcda88379009c569d33ab5d63ba6405b022
Jeremy Peters <info.peters@quantentunnel.de>
2020-02-03 18:04:37 +0100

    Add a verbose option and distinguishable log messages for multiple instances

--- src/ipc/worker.py
+++ src/ipc/worker.py
@@ -4,8 +4,11 @@ import inspect
 from subprocess import Popen, PIPE
 from threading import Thread, Event
 
+
 class Worker:
-    def __init__(self, command, callback=None, start=True):
+    __id = 0
+
+    def __init__(self, command, callback=None, start=True, verbose=False):
         if isinstance(command, str):
             command = shlex.split(command, posix=False)
         elif not isinstance(command, list):
@@ -21,10 +24,15 @@ class Worker:
                 arg_amount = len(inspect.getargspec(callback).args)
             if arg_amount != 1:
                 raise TypeError('Callback must take exactly one argument!')
+        Worker.__id += 1
+        self.__id = Worker.__id
         self.__cmd = command
         self.__callback = callback
+        self.__verbose = verbose
         self.__data = None
         self.__process = None
+        self.__process_thread = None
+        self.__read_thread = None
         self.__running = Event()
         if start:
             self.start()
@@ -35,6 +43,9 @@ class Worker:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
 
+    def __del__(self):
+        self.stop()
+
     @property
     def running(self):
         return self.__running.is_set() and self.__process and self.__process.returncode is None
@@ -43,47 +54,63 @@ class Worker:
     def data(self):
         return self.__data
 
+    def __print(self, text):
+        print('[IPC Worker {}] {}'.format(self.__id, text))
+
+    def __log(self, text):
+        if self.__verbose:
+            self.__print(text)
+
     def __read(self):
         while self.running:
             try:
                 readout = self.__process.stdout.readline().strip()
                 if readout:
                     self.__data = readout.decode()
+                    self.__log('Received: {}'.format(self.__data))
                     if self.__callback:
                         self.__callback(self.__data)
             except Exception:
-                print('Error reading data!')
+                self.__print('Error reading data!')
 
     def __start(self):
-        print('Started worker')
         exit_code = self.__process.wait()
+        self.__log('Program ended with exit code: {}'.format(exit_code))
         self.stop()
-        print('Stopped worker [exit code: {}]'.format(exit_code))
 
     def start(self):
+        self.__log('Started worker')
         if not self.running:
             try:
                 self.__process = Popen(self.__cmd, stdin=PIPE, stdout=PIPE)
             except Exception as e:
-                print('Error opening "{}": {}'.format(' '.join(self.__cmd), e.strerror))
+                self.__print('Error opening "{}": {}'.format(' '.join(self.__cmd), e.strerror))
                 return
             self.__running.set()
-            Thread(target=self.__start).start()
-            Thread(target=self.__read).start()
+            self.__process_thread = Thread(target=self.__start).start()
+            self.__read_thread = Thread(target=self.__read).start()
 
     def send(self, data):
         if self.running:
+            self.__log('Sending: {}'.format(data))
             try:
                 self.__process.stdin.write((str(data) + '\n').encode())
                 self.__process.stdin.flush()
             except (OSError, IOError):
-                print('Error sending data!')
+                self.__print('Error sending data!')
         else:
-            print('Worker not running!')
+            self.__log('Worker not running!')
 
     def stop(self):
         if self.running:
             self.__process.terminate()
         self.__running.clear()
-        self.__process = None
+        if self.__process_thread:
+            self.__process_thread.join()
+        if self.__read_thread:
+            self.__read_thread.join()
         self.__data = None
+        self.__process = None
+        self.__process_thread = None
+        self.__read_thread = None
+        self.__log('Stopped worker')
